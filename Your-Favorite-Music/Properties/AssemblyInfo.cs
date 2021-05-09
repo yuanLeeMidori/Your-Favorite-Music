a9f8805d65b108870b91544fe7b558a497b7959d
Yuan-Hsi Lee <yuanc.lee.tw@gmail.com>
2021-05-08 22:11:08 -0400

    Rename to YFM

--- Your-Favorite-Music/Properties/AssemblyInfo.cs
+++ Your-Favorite-Music/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignment6")]
+[assembly: AssemblyTitle("YFM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Assignment6")]
+[assembly: AssemblyProduct("YFM")]
 [assembly: AssemblyCopyright("Copyright ©WEB524 Fall 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
